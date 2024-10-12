2037cd8ba95da847eb5009fd16a40b11bfa80025
tulik <tolqinmirsaliyev@gmail.com>
2024-10-12 14:04:11 -0500

    dsdsdsd

--- index.js
+++ index.js
@@ -29,6 +29,8 @@ app.use(cors({
 // app.use(cors(corsOptions));
 
 app.use(express.static(path.join(__dirname, 'uploads')));
+app.use(express.json());
+
 
 // Routes
 app.use('/api',  questionRoutes)
