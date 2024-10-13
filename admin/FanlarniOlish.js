30519e192237122303a2b0e509f7927a07feeabd
tulik <tolqinmirsaliyev@gmail.com>
2024-10-12 21:34:08 -0500

    sssss

--- admin/FanlarniOlish.js
+++ admin/FanlarniOlish.js
@@ -23,7 +23,29 @@ const getSubjects = async (req, res) => {
   }
 };
 
+const getAllSubjects = async (req, res) => {
+  // Bodydan fanId ni olamiz
+
+
+  try {
+    let subjects;
+    if (fanId) {
+      // fanId bo'yicha fanlarni olamiz
+      subjects = await Subject.find(); // Faqat fan nomini olamiz
+    } else {
+      return res.status(400).json({ message: 'Fanlar topilmadi!' });
+    }
+
+
+    res.status(200).json({ subjects }); // Fanlarni muvaffaqiyatli qaytarish
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Fanlarni olishda xato yuz berdi!' });
+  }
+};
+
 
 module.exports = {
   getSubjects,
+  getAllSubjects
 };
--- routes/fanlarOlish.js
+++ routes/fanlarOlish.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getSubjects } = require('../admin/FanlarniOlish');
+const { getSubjects, getAllSubjects } = require('../admin/FanlarniOlish');
 
 // Barcha fanlarni yoki adminId bo'yicha fanlarni olish
-router.get('/subjects', getSubjects);
+router.get('/subjects', getAllSubjects);
 
 router.get('/subjects/:id', getSubjects);// adminId ixtiyoriy
 
