1f3c1c0c50e5a0a4a224e12e1bbde503ed453e54
tulik <tolqinmirsaliyev@gmail.com>
2024-10-12 21:39:57 -0500

    asasa

--- admin/FanlarniOlish.js
+++ admin/FanlarniOlish.js
@@ -1,10 +1,9 @@
 const Subject = require('../Model/Fanlar'); // Subject modelini import qilamiz
 const Admin = require('../Model/adminlar'); // Admin modelini import qilamiz
 
-// Fanlar ro'yxatini olish va adminId bo'yicha filtrlash
+// Fanlar ro'yxatini olish va fanId bo'yicha filtrlash
 const getSubjects = async (req, res) => {
-  const { fanId } = req.params; // Bodydan fanId ni olamiz
-
+  const { fanId } = req.params; // Paramsdan fanId ni olamiz
 
   try {
     let subjects;
@@ -15,7 +14,6 @@ const getSubjects = async (req, res) => {
       return res.status(400).json({ message: 'FanId berilmagan!' });
     }
 
-
     res.status(200).json({ subjects }); // Fanlarni muvaffaqiyatli qaytarish
   } catch (error) {
     console.error(error);
@@ -23,19 +21,11 @@ const getSubjects = async (req, res) => {
   }
 };
 
+// Hamma fanlar ro'yxatini olish
 const getAllSubjects = async (req, res) => {
-  // Bodydan fanId ni olamiz
-
-
   try {
-    let subjects;
-    if (fanId) {
-      // fanId bo'yicha fanlarni olamiz
-      subjects = await Subject.find(); // Faqat fan nomini olamiz
-    } else {
-      return res.status(400).json({ message: 'Fanlar topilmadi!' });
-    }
-
+    // Hamma fanlarni olamiz
+    const subjects = await Subject.find().select('name'); // Faqat fan nomlarini olamiz
 
     res.status(200).json({ subjects }); // Fanlarni muvaffaqiyatli qaytarish
   } catch (error) {
@@ -43,7 +33,6 @@ const getAllSubjects = async (req, res) => {
     res.status(500).json({ message: 'Fanlarni olishda xato yuz berdi!' });
   }
 };
-
 
 module.exports = {
   getSubjects,
