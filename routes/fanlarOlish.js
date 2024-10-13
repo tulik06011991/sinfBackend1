600051b603d5177b2873fd878c663b536c9c15d7
tulik <tolqinmirsaliyev@gmail.com>
2024-10-12 21:27:57 -0500

    fdfdfd

--- routes/fanlarOlish.js
+++ routes/fanlarOlish.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const { getSubjects } = require('../admin/FanlarniOlish');
 
 // Barcha fanlarni yoki adminId bo'yicha fanlarni olish
-// router.get('/subjects', getSubjects);
+router.get('/subjects', getSubjects);
 
-router.get('/subjects', getSubjects);// adminId ixtiyoriy
+router.get('/subjects/:id', getSubjects);// adminId ixtiyoriy
 
 module.exports = router;
